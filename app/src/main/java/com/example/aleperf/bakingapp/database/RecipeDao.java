d1c4234b6304b2b8b5dc19fb1a022a87231e889f
aleperf <aleperf@users.noreply.github>
2018-03-15 14:44:20 +0100

    feat: add more methods do database dao

--- app/src/main/java/com/example/aleperf/bakingapp/database/RecipeDao.java
+++ app/src/main/java/com/example/aleperf/bakingapp/database/RecipeDao.java
@@ -2,11 +2,14 @@ package com.example.aleperf.bakingapp.database;
 
 import android.arch.lifecycle.LiveData;
 import android.arch.persistence.room.Dao;
+import android.arch.persistence.room.Insert;
 import android.arch.persistence.room.Query;
 
 import com.example.aleperf.bakingapp.model.Recipe;
 
 import java.util.List;
+
+import static android.arch.persistence.room.OnConflictStrategy.REPLACE;
 
 @Dao
 public interface RecipeDao {
@@ -16,4 +19,10 @@ public interface RecipeDao {
 
     @Query("SELECT * FROM Recipe.RECIPES_TABLE WHERE id = :id LIMIT 1")
     Recipe getRecipeWithId(int id);
+
+    @Insert(onConflict = REPLACE)
+    void insertRecipes(List<Recipe> recipes);
+
+    @Insert(onConflict = REPLACE)
+    void insertRecipe(Recipe recipe);
 }
