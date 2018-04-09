9ca041ac4f7c81146f937c5ed103b6a8178ab35a
aleperf <aleperf@users.noreply.github>
2018-04-09 10:19:31 +0200

    feat: add maintaining ingredient list expansion state on rotation

--- app/src/main/java/com/example/aleperf/bakingapp/ui/recipeDetail/RecipeSummaryFragment.java
+++ app/src/main/java/com/example/aleperf/bakingapp/ui/recipeDetail/RecipeSummaryFragment.java
@@ -8,23 +8,27 @@ import android.arch.lifecycle.ViewModelProviders;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.constraint.ConstraintLayout;
+import android.support.transition.AutoTransition;
+
+import android.support.transition.TransitionManager;
+
 import android.support.v4.app.Fragment;
+import android.support.v4.content.ContextCompat;
+
+import android.support.v7.widget.DividerItemDecoration;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
 import android.widget.ImageButton;
 import android.widget.TextView;
 
 import com.example.aleperf.bakingapp.BakingApplication;
 import com.example.aleperf.bakingapp.R;
 import com.example.aleperf.bakingapp.model.Recipe;
-import com.example.aleperf.bakingapp.ui.recipeDetail.IngredientsAdapter;
-import com.example.aleperf.bakingapp.ui.recipeDetail.RecipeDetailViewModel;
-
-import java.util.List;
 
 import javax.inject.Inject;
 
@@ -33,12 +37,13 @@ import butterknife.ButterKnife;
 import butterknife.Unbinder;
 
 /**
- * Shows, briefly, ingredients and summary of descriptions of selected recipe's steps.
+ * Shows ingredients and summary of descriptions of selected recipe's steps.
  */
 
 public class RecipeSummaryFragment extends Fragment {
 
     private static final String RECIPE_EXTRA_ID = "recipe extra id";
+    private static final String INGREDIENTS_EXPANSION_STATE = "ingredients expansion state";
 
     @BindView(R.id.ingredients_title_text_view)
     TextView ingredientTitle;
@@ -48,6 +53,8 @@ public class RecipeSummaryFragment extends Fragment {
     ImageButton buttonExpand;
     @BindView(R.id.button_hide)
     ImageButton buttonHide;
+    @BindView(R.id.ingredients_section)
+    ConstraintLayout ingredientsSection;
 
     RecipeDetailViewModel model;
     @Inject
@@ -56,24 +63,30 @@ public class RecipeSummaryFragment extends Fragment {
     LiveData<Recipe> recipe;
 
     private Unbinder unbinder;
-
+    private boolean isExpandedIngredientList = false;
 
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         ((BakingApplication) getActivity().getApplication()).getBakingApplicationComponent().inject(this);
 
-    }
+        }
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View root = inflater.inflate(R.layout.fragment_recipe_summary, container, false);
         unbinder = ButterKnife.bind(this, root);
+        if(savedInstanceState != null){
+            isExpandedIngredientList = savedInstanceState.getBoolean(INGREDIENTS_EXPANSION_STATE, false);
+        }
         LinearLayoutManager ingredientManager = new LinearLayoutManager(getActivity());
         ingredientList.setLayoutManager(ingredientManager);
         ingredientsAdapter = new IngredientsAdapter(getActivity());
         ingredientList.setAdapter(ingredientsAdapter);
+        addItemDecorator(ingredientList);
+        setIngredientsExpansionListener();
+
         return root;
 
     }
@@ -92,21 +105,74 @@ public class RecipeSummaryFragment extends Fragment {
             @Override
             public void onChanged(@Nullable Recipe recipe) {
                 if (recipe != null) {
-                    Log.d("uffa", "recipe non è null");
-                    ingredientTitle.setText(recipe.getName());
+                    ingredientTitle.setText(recipe.getName() + getString(R.string.recipe_ingredient));
                     ingredientsAdapter.setIngredients(recipe);
-                    List<Recipe.Ingredient> ingredients = recipe.getIngredients();
-                    Log.d("uffa", "ingredients size = " + ingredients.size());
-                    Log.d("uffa", "primo ingrediente " + ingredients.get(0).getIngredient());
-
-                } else {
-                    Log.d("uffa", "recipe è null");
-                }
+                    }
             }
         };
-
         recipe.observe(this, observer);
+    }
 
+
+
+    /**
+     * Add a divider decoration to a RecyclerView
+     */
+
+    private void addItemDecorator(RecyclerView recyclerView) {
+        DividerItemDecoration itemDecorator = new DividerItemDecoration(getActivity(), DividerItemDecoration.VERTICAL);
+        itemDecorator.setDrawable(ContextCompat.getDrawable(getActivity(), R.drawable.recyclerview_divider));
+        recyclerView.addItemDecoration(itemDecorator);
+    }
+
+    private void setIngredientsExpansionListener(){
+
+        View.OnClickListener listener = new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                TransitionManager.beginDelayedTransition(ingredientsSection, new AutoTransition().setDuration(400));
+                setUpIngredientListAppearance(isExpandedIngredientList);
+                isExpandedIngredientList = !isExpandedIngredientList;
+                }
+        };
+        buttonExpand.setOnClickListener(listener);
+        buttonHide.setOnClickListener(listener);
+    }
+
+    /**
+     * Change the Ingredients List appearance according to its expansion state
+     * @param isExpanded a boolean representing the state of ingredient list, true if expanded, false if not
+     */
+
+    public void setUpIngredientListAppearance(boolean isExpanded){
+
+        if(isExpanded){
+            ingredientList.setVisibility(View.GONE);
+            buttonHide.setVisibility(View.GONE);
+            buttonExpand.setVisibility(View.VISIBLE);
+
+
+        } else {
+            ingredientList.setVisibility(View.VISIBLE);
+            buttonExpand.setVisibility(View.GONE);
+            buttonHide.setVisibility(View.VISIBLE);
+
+
+        }
+
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        setUpIngredientListAppearance(!isExpandedIngredientList);
+    }
+
+    @Override
+    public void onSaveInstanceState(@NonNull Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putBoolean(INGREDIENTS_EXPANSION_STATE, isExpandedIngredientList);
     }
 
     @Override
@@ -114,6 +180,4 @@ public class RecipeSummaryFragment extends Fragment {
         super.onDestroyView();
         unbinder.unbind();
     }
-
-
 }
