d57aa071e8bdf58419a5971fe33495eee6389a9b
aleperf <aleperf@users.noreply.github>
2018-05-07 05:55:49 +0200

    refactor RecipesViewModel

--- app/src/main/java/com/example/aleperf/bakingapp/ui/intro/RecipesViewModel.java
+++ app/src/main/java/com/example/aleperf/bakingapp/ui/intro/RecipesViewModel.java
@@ -14,6 +14,7 @@ import javax.inject.Inject;
 public class RecipesViewModel extends ViewModel {
 
     private static final String TAG = RecipesViewModel.class.getSimpleName();
+    private LiveData<List<Recipe>> recipes;
 
     private RecipeRepository repository;
 
@@ -25,7 +26,9 @@ public class RecipesViewModel extends ViewModel {
     }
 
     public LiveData<List<Recipe>> getRecipes(){
-
-        return repository.getAllRecipes();
+        if(recipes == null){
+            recipes = repository.getAllRecipes();
+        }
+        return recipes;
     }
 }
