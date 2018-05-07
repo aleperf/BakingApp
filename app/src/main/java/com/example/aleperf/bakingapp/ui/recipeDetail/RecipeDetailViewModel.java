d9aae7f465d96be550ceff0892cccacbbe4436eb
aleperf <aleperf@users.noreply.github>
2018-05-07 06:02:49 +0200

    refactor RecipesDetailViewModel

--- app/src/main/java/com/example/aleperf/bakingapp/ui/recipeDetail/RecipeDetailViewModel.java
+++ app/src/main/java/com/example/aleperf/bakingapp/ui/recipeDetail/RecipeDetailViewModel.java
@@ -1,6 +1,7 @@
 package com.example.aleperf.bakingapp.ui.recipeDetail;
 
 import android.arch.lifecycle.LiveData;
+import android.arch.lifecycle.MutableLiveData;
 import android.arch.lifecycle.ViewModel;
 import com.example.aleperf.bakingapp.database.RecipeRepository;
 import com.example.aleperf.bakingapp.model.Recipe;
@@ -11,16 +12,23 @@ public class RecipeDetailViewModel extends ViewModel {
 
     private final String TAG = RecipeDetailViewModel.class.getSimpleName();
     private RecipeRepository repository;
+    private LiveData<Recipe> recipe;
+    private int recipeId = -1;
 
 
 
     @Inject
     public RecipeDetailViewModel(RecipeRepository repository){
         this.repository = repository;
-    }
+        }
 
     public LiveData<Recipe> getRecipe(int recipeId){
-      return repository.getRecipeWithId(recipeId);
+       if(recipe != null && this.recipeId == recipeId){
+           return recipe;
+       }
+        this.recipeId = recipeId;
+        recipe = repository.getRecipeWithId(recipeId);
+        return recipe;
     }
 
 
